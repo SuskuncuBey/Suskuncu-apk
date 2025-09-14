9615d15247f99a3a1cfb56de00722ba6ba869beb
SuskuncuBey <telegramcibaba@gmail.com>
2025-09-14 06:12:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ package.json   ← Proje ve start komutu bilgisi
 ├─ index.html
 └─ apk/
     └─ benim-konusan-tomum-hileli-apkdayi.com.apk
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
